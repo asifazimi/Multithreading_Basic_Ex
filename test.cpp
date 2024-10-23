8e6bf550eb5d690510e45301a5268f1b5f92cae2
asifazimi <asifazimi@outlook.com>
2024-10-23 15:06:57 +0200

    SLURM added

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <map>
+#include <string>
+#include <chrono>
+using namespace std::chrono_literals;
+
+// Refresh the tempreture after 2000ms
+void RefreshForecast(std::map<std::string, int> forecastMap)
+{
+       // To repeat refresh
+       while (true)
+       {
+              for (auto &item : forecastMap)
+              {
+                     item.second++;
+                     std::cout << item.first << " - " << item.second << std::endl;
+              }
+              // sleep after refreshing
+              std::this_thread::sleep_for(2000ms);
+       }
+}
+
+int main()
+{
+       // Weather API
+       std::map<std::string, int> forecastMap = {
+           {"New York", 15},
+           {"Vienna", 20},
+           {"Berline", 10},
+       };
+
+       std::thread bgWorker(RefreshForecast, forecastMap);
+
+       system("pause>null");
+}
