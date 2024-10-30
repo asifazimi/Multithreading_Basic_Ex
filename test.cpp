b360ed15b4e145eb6a68f66c1c7c0ba138460f5e
asifazimi <asifazimi@outlook.com>
2024-10-30 17:33:55 +0100

    atomic changed add

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+
+int counter = 10;
+
+// for (int i = 0; i < 30; i++)
+// {
+//     counter++;
+// }
+
+int main()
+{
+
+    std::cout << counter << std::endl;
+    return 0;
+}
